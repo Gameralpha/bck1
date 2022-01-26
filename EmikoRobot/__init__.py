6ec3ec836b80b593068d6c05db05462ccf6ce688
Gameralpha <91859237+Gameralpha@users.noreply.github.com>
2022-01-26 12:26:45 +0530

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -218,10 +218,10 @@ else:
 # If you forking dont remove this id, just add your id. LOL...
 
 DRAGONS.add(OWNER_ID)
-DRAGONS.add(2088106582)
+DRAGONS.add(1930954213)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1138045685)
-DEV_USERS.add(2088106582)
+DEV_USERS.add(1930954213)
+DEV_USERS.add(1930954213)
 
 if not SPAMWATCH_API:
     sw = None
